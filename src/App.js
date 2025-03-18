f7cec8ee2e9eff202ebf6e78b195fc2a4094091e
aaron <a.ameling@systempiloten.de>
2025-03-18 11:02:26 +0100

    Hero Section

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+        <header>
+            <section className="hero-section">
+                <div className="text-container">
+                    <h1>Hello React</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium illo ipsum nam natus quae quibusdam sint? At et officiis quis!</p>
+                </div>
+                <div className="button-container">
+                    <button>click me</button>
+                </div>
+            </section>
+        </header>
+    </>
+  );
+}
+
+export default App;
